d7bd29e474840e4e50fe31d31f21ef746de54dff
GitHub Action <action@github.com>
2023-07-23 10:02:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/rrtt2323/quicklog/MyBundle.kt
+++ src/main/kotlin/com/github/rrtt2323/quicklog/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rrtt2323.quicklog
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/rrtt2323/quicklog/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/rrtt2323/quicklog/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rrtt2323.quicklog.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
